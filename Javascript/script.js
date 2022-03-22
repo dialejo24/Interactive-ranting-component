654f529127440eecc12a547dd0e4850e3193f016
dialejo24 <dialejosi530@gmail.com>
2022-03-21 19:53:35 -0500

    some javascript added

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,2 +1,29 @@
-const card = document.querySelector(".card");
-console.log(card);
+const form = document.querySelector("form");
+const inputs = document.querySelectorAll("input");
+const userRank = document.querySelector(".user-rank");
+
+function getUserRank(e){
+    e.preventDefault();
+    userRank.textContent = "You selected"
+    for(let i = 0; i < inputs.length; i++){
+        if(inputs[i].checked){
+            userRank.textContent = userRank.textContent + " " + inputs[i].value + " out of 5";
+            return;
+        }
+    }
+    alert("Please, select a rank");
+}
+
+function setLabelFocused(e){
+    e.target.parentElement.classList.add("label-focus");
+    inputs.forEach(entrada => {
+        if(entrada != e.target && entrada.parentElement.classList.length != 0){
+            entrada.parentElement.classList.remove("label-focus");
+        }
+    })
+}
+
+
+form.addEventListener("submit", getUserRank);
+
+inputs.forEach(input => input.addEventListener("click", setLabelFocused));
